92c06a64ecb38ae5e341af40b682be8698ec6891
Julia <juliageist23@gmail.com>
2017-03-22 19:02:33 -0700

    api now accepts json post requests

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,12 +1,15 @@
 var request = require('request');
 
+// uses the PORT environment variable, if available, otherwise uses port 3000
+var port = process.env.PORT || 3000;
+
 describe("POST to /", function() {
     it("should extract time and render html", function(done) {
         request({
-		    url: "http://localhost:3000/",
+		    url: "http://localhost:" + port,
 		    method: "POST",
 		    json: true,   
-		    body: {text: "I have to do something by tomorrow at 8am"}}
+		    body: {text: "I have to do something by tomorrow at 8am"}
 		}, function (error, response, body){
 			// expect time to have been extracted
 			done();
