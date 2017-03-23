811ab650bbd3c5030428fd35772da0abf3203a17
Julia <juliageist23@gmail.com>
2017-03-22 18:40:31 -0700

    update spec, rm commented code

--- routes/home.js
+++ routes/home.js
@@ -12,23 +12,12 @@ module.exports = function(app) {
 
 	app.post('/', urlencodedParser, function(req, res){
 		// render extractedDate html file (specified ejs extension in app.js)
-		
+
 
 		var date; 
 
 		res.render('extractedDate', { TEXTSTR: req.body.text,
 									  DATE: date });
 	});
-
-	// app.post('/extract', function(req, res){
-	// 	// get body of post req
-	// 	var textString = req.params.text;
-
-	// 	// make api request to duckling
-
-	// 	// write response of api to display in html page
-	// 	// res.send( <HTML DOC> );
-	// 	res.render('form');
-	// });
 
 };
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,16 +1,14 @@
 var request = require('request');
 
-describe("test test", function() {
-    it("should test something", function(done) {
+describe("POST to /", function() {
+    it("should extract time and render html", function(done) {
         request({
 		    url: "http://localhost:3000/",
 		    method: "POST",
 		    json: true,   
-		    body: user
+		    body: {text: "I have to do something by tomorrow at 8am"}}
 		}, function (error, response, body){
-			expect(body).toEqual(JSON.stringify(user[uid]));
-			// expect stauts code 201
-			// expect(response.statusCode).toBe(201);
+			// expect time to have been extracted
 			done();
 		});
     });
